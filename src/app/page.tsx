cb27ee410913b08457664cb8742f209fb3173abd
SurlaGit <carlossurla26@gmail.com>
2023-06-15 18:53:00 +0800

    fix import hierarchy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
+import Image from 'next/image'
 import AboutMe from '@/components/AboutMe'
 import VincentComponent from '@/components/VincentComponent'
-import Image from 'next/image'
+
 
 export default function Home() {
   return (
