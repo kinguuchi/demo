3e7ec09ba40ee9511ff5fd6c72ada3c2a70625ad
RuruCodes <Ruelbares888@gmail.com>
2023-06-30 23:50:50 +0800

    New page with centered button

---
+++ src/app/ruel/page.tsx
@@ -0,0 +1,11 @@
+const Ruel = () => {
+    return(
+        <div  className="w-screen h-screen flex items-center justify-center">
+        <button className="bg-blue-500 text-white font-bold">
+            Click Me!
+        </button>
+        </div>
+    )
+}
+
+export default Ruel;
