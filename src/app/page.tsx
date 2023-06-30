974b1f43eb0152bcadc7298fe20cc44f393cc163
BastyVovo <bastyaccnt.0@gmail.com>
2023-06-30 23:15:15 +0800

    Tapos na me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Image from 'next/image'
 import AboutMe from '@/components/AboutMe'
 import VincentComponent from '@/components/VincentComponent'
+import Btn from '@/components/BastyComponent'
 
 
 export default function Home() {
   return (
     <div>
       <AboutMe />
-    <VincentComponent/>
+      <VincentComponent/>
+      <Btn />
     </div>
   )
 }
