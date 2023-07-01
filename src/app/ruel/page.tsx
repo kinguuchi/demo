33b2d7209271cbcd0570d4d241f8ba388185cdb1
RuruCodes <Ruelbares888@gmail.com>
2023-07-01 09:17:42 +0800

    Component Added

--- src/app/ruel/page.tsx
+++ src/app/ruel/page.tsx
@@ -1,7 +1,7 @@
 const Ruel = () => {
     return(
         <div  className="w-screen h-screen flex items-center justify-center">
-        <button className="bg-blue-500 text-white font-bold">
+        <button className="bg-blue-500 text-white font-bold hover:bg-blue-900 rounded-full border border-black animate-bounce">
             Click Me!
         </button>
         </div>
